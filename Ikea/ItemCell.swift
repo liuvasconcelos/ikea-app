d7bb69531a4401777df3f97e4ff540add4eb2db5
liuvasconcelos <livia.vasconcelos@stant.com.br>
2020-01-02 20:58:53 -0300

    develop Add initial structure.
    - Add collection view interaction.
    
    Signed-off-by: liuvasconcelos <livia.vasconcelos@stant.com.br>

---
+++ Ikea/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  Ikea
+//
+//  Created by Livia Vasconcelos on 02/01/20.
+//  Copyright © 2020 Livia Vasconcelos. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+}
--- Ikea/ViewController.swift
+++ Ikea/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var itemsCollectionView: UICollectionView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    let itemsArray: [String] = ["Cup", "Vase", "Boxing", "Table"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sceneView.debugOptions = [SCNDebugOptions.showWorldOrigin,
+                                  SCNDebugOptions.showFeaturePoints]
+        sceneView.session.run(configuration)
+        
+        self.itemsCollectionView.dataSource = self
+        self.itemsCollectionView.delegate   = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as? ItemCell
+        cell?.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell ?? UICollectionViewCell()
     }
 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = .green
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = .yellow
+    }
 
 }
 
